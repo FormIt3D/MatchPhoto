8b06c839b9b352b6f719e1880b63db5565204cb5
Josh Goldstein <josh.goldstein@autodesk.com>
2021-11-30 20:18:01 -0700

    Converting to web- and FormIt-side scripts for better performance

---
+++ MatchPhoto_Client.js
@@ -0,0 +1,77 @@
+var MatchPhoto = MatchPhoto || {};
+
+/*** application code - runs asynchronously from plugin process to communicate with FormIt ***/
+
+MatchPhoto.stringAttributeKey = 'FormIt::Plugins::MatchPhoto';
+
+MatchPhoto.updatePhotoObjectToMatchCamera = function()
+{
+    var nEditingHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
+
+    // first, check if a match photo object already exists in this history
+    var aInstancesWithStringAttribute = FormIt.PluginUtils.Application.getGroupInstancesByStringAttributeKey(nEditingHistoryID, MatchPhoto.stringAttributeKey);
+    var bMatchPhotoObjectExists = aInstancesWithStringAttribute.length > 0;
+
+    // if the match photo object exists, move it to face the camera
+    if (bMatchPhotoObjectExists)
+    {
+        var cameraObjectInstanceID = aInstancesWithStringAttribute[0];
+        var cameraObjectHistoryID = WSM.APIGetGroupReferencedHistoryReadOnly(nEditingHistoryID, cameraObjectInstanceID);
+        var cameraObjectInstanceTransf3d = WSM.APIGetInstanceTransf3dReadOnly(nEditingHistoryID, cameraObjectInstanceID);
+
+        // re-create the instance transform without any scaling
+        var cameraObjectCoordinateSystem = WSM.Transf3d.GetCoordinateSystem(cameraObjectInstanceTransf3d);
+        var xDirNormalized = WSM.Vector3d.GetNormalized(cameraObjectCoordinateSystem.xDir);
+        var yDirNormalized = WSM.Vector3d.GetNormalized(cameraObjectCoordinateSystem.yDir);
+        var newInstanceTransform = WSM.Transf3d.Transf3d(cameraObjectCoordinateSystem.origin, xDirNormalized, yDirNormalized);
+
+        var cameraObjectInvertedTransform = WSM.Geom.InvertTransform(newInstanceTransform);
+
+        // get camera data
+        var cameraForward = FormIt.Cameras.GetCameraWorldForward();
+        var cameraUp = FormIt.Cameras.GetCameraWorldUp();
+        var cameraPosition = FormIt.Cameras.GetCameraWorldPosition();
+        var cameraRight = WSM.Vector3d.CrossProduct(cameraForward, cameraUp);
+        cameraUp = WSM.Vector3d.MultiplyByFactor(cameraUp, -1);
+        var cameraTransform = WSM.Transf3d.Transf3d(cameraPosition, cameraRight, cameraUp);
+
+        // calculate the transform to get to the current camera
+        var finalTransform = WSM.Transf3d.Multiply(cameraTransform, cameraObjectInvertedTransform);
+
+        // apply the transform to the camera object
+        WSM.APITransformObject(nEditingHistoryID, cameraObjectInstanceID, finalTransform);	
+    }
+    // otherwise, make it from scratch
+    else
+    {
+        var cameraData = FormIt.Cameras.GetCameraData();
+    
+        var viewportSize = FormIt.Cameras.GetViewportSize();
+        var aspectRatio = viewportSize.width / viewportSize.height;
+    
+        var matchPhotoObjectInstanceID = ManageCameras.createCameraGeometryFromCameraData(nEditingHistoryID, cameraData, aspectRatio);
+
+        WSM.Utils.SetOrCreateStringAttributeForObject(nEditingHistoryID,
+            matchPhotoObjectInstanceID, MatchPhoto.stringAttributeKey, "Test!");
+
+        //put the instance on a layer and lock the layer
+        
+    }
+
+}
+
+// set up the message listener
+MessagesPluginListener = {};
+MessagesPluginListener.MsgHandler = function(msg, payload) { 
+
+    MatchPhoto.updatePhotoObjectToMatchCamera();
+
+};
+// Create a Message Listener that handles calling the subscribed message handlers.
+if (!(MessagesPluginListener.hasOwnProperty("listener")))
+{
+    MessagesPluginListener.listener = FormIt.Messaging.NewMessageListener();
+}
+// Assign the msg handler
+MessagesPluginListener.listener["FormIt.Message.kCameraChanged"] = MessagesPluginListener.MsgHandler;
+MessagesPluginListener.listener.SubscribeMessage("FormIt.Message.kCameraChanged");
---
+++ MatchPhoto_Web.js
@@ -0,0 +1,24 @@
+window.MatchPhoto = window.MatchPhoto || {};
+
+/*** web/UI code - runs natively in the plugin process ***/
+
+// initialize the UI
+MatchPhoto.initializeUI = async function()
+{
+    // create an overall container for all objects that comprise the "content" of the plugin
+    // everything except the footer
+    let contentContainer = document.createElement('div');
+    contentContainer.id = 'contentContainer';
+    contentContainer.className = 'contentContainer'
+    window.document.body.appendChild(contentContainer);
+
+    // create the overall header
+    let headerContainer = new FormIt.PluginUI.HeaderModule('Match Photo', 'Match a photo to the 3D scene.', 'headerContainer');
+    contentContainer.appendChild(headerContainer.element);
+
+    // separator and space
+    contentContainer.appendChild(document.createElement('hr'));
+    contentContainer.appendChild(document.createElement('p'));
+}
+
+
